bbf01f40a4e6cf8f52f377b97435f1384724a6c7
Martin Petrov <martin.petrov@nordcloud.com>
2023-01-12 18:43:28 +0200

    update the config once again

--- src/config.ts
+++ src/config.ts
@@ -2,12 +2,12 @@ import { Config } from './constructs/Scheduler/types';
 
 // Date format is "2023-01-13T10:30Z",
 // Time zone is UTC
-// Increments of 30 minutes only
+// Whole and half hours only.
 
 export const configEntries: Config = [
   {
     projectSlug: 'skf-dmc-bearing-backend',
     clientKey: 'd835311c799b4743a7b83e7258663315',
-    downtimePeriod: ['2023-01-12T16:30Z', '2023-01-12T17:00Z'],
+    downtimePeriod: ['2023-01-12T17:00Z', '2023-01-12T17:30Z'],
   },
 ];
